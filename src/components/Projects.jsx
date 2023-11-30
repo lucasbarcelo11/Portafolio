39c793a0e8ee65cdc753771ec1d3b4065cba2cdd
lucasb <lucasbarcelo1981@gmail.com>
2023-11-30 12:32:50 -0300

    link actualizado

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
       <h1 className='text-4xl font-bold text-center text-[#001b5e] dark:text-gray-200'>Proyectos</h1>
       <p className='text-center py-8 dark:text-gray-200'>Proyectos realizados con JavaScript, html 5, css3, React, Next.js, Boostrap & Tailwind</p>
       <div className='grid sm:grid-cols-2 gap-12'>
-        <ProjectsItems img={'/myfinanz.png'} title='My Finanz con Next.js' href={'https://c14-17-ft-node-react.vercel.app/'} linkGit={'https://github.com/No-Country/c14-17-ft-node-react'}/>
+        <ProjectsItems img={'/myfinanz.png'} title='My Finanz con Next.js' href={'https://mifinanz.vercel.app/'} linkGit={'https://github.com/No-Country/c14-17-ft-node-react'}/>
         <ProjectsItems img={'/ecommerce.png'} title='e-commerce con React' href={'https://ecommercelucasbarcelo.netlify.app/'} linkGit={'https://github.com/lucasbarcelo11/e-commerce'}/>
         <ProjectsItems img={'/Clima.png'} title='App Clima' href={'https://weather-projectoacademlo.netlify.app/'} linkGit={'https://github.com/lucasbarcelo11/weather-project'}/>
         <ProjectsItems img={'/e-commerceAcademlo.png'} title='e-commerce JavaScript' href={'https://carrito-g27-lucasygustavo.netlify.app/'} linkGit={'https://github.com/lucasbarcelo11/carg27'}/>
