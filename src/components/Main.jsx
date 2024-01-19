38978af37251789d23ed4d121231d0dfdb818cc9
lucasb <lucasbarcelo1981@gmail.com>
2024-01-19 20:44:33 -0300

    cambiando de curriculum y tama;o de iconos

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
     window.open('https://github.com/lucasbarcelo11', '_blank');
   }
 
-  const download = ('/CVLucasBarcelo!.pdf')
+  const download = ('/LucasBarceloCV.pdf')
 
 
   
@@ -57,8 +57,8 @@ const Main = () => {
             />
           </h2>
           <div className="flex justify-between pt-6 max-w-[100px] w-full dark:text-gray-200">
-            <FaLinkedin className="cursor-pointer" size={20} onClick={linkedin} target="_blank" />
-            <FaGithub className="cursor-pointer" size={20} onClick={github} />
+            <FaLinkedin className="cursor-pointer" size={30} onClick={linkedin} target="_blank" />
+            <FaGithub className="cursor-pointer" size={30} onClick={github} />
           </div>
           <div className="pt-10 flex justify-center gap-3">
           <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "><a download={download} href={download}>Curriculum</a></button>
